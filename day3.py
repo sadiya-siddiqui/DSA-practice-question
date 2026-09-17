ba283bebd9e1cde9a55e9bf7fc98afa762452b59
sadiya-siddiqui <siddiquisadiya874@gmail.com>
2026-09-17 22:37:16 +0530

    day 3 DSA practice

---
+++ day3.py
@@ -0,0 +1,53 @@
+
+##Q7
+# a=[30,20,4,5,6]
+# total=0
+# for i in  a:
+#     total=total+i
+# print(total)
+
+##Q8
+# a=[10,40,30,50,30,7]
+# count=0
+# for i in a :
+#     if i%2!=0:
+#      count+=1
+# print(count)
+
+##Q9
+# a=[30,20,50,3,5]
+# b=int(input("enter no."))
+# found=False
+# for i in a:
+#     if  i==b:
+#         found=True
+#         break
+
+# if found:
+#     print("found")
+# else:
+#     print("not found" )
+
+##Q10
+# a=[10,20,30,40]
+# b=[]
+# for i in a:
+#     c=i*2
+#     b.append(c)
+# print(b)
+
+##Q11
+# a=[10,70,20,90,45,100,3]
+# count_largest=0
+# for i in a:
+#     if i>50:
+#         count_largest+=1
+# print(count_largest)
+
+##Q12
+a=[40,20,5,40,6,0]
+b=[]
+for i in a:
+    if i%2==0:
+        b.append(i)
+print(b)
