2508cb76eda4812fc1cb08bf2d7f85db8f6e685f
sadiya-siddiqui <siddiquisadiya874@gmail.com>
2026-09-23 23:15:09 +0530

    DSA practice

---
+++ day9.py
@@ -0,0 +1,92 @@
+##Q1
+# a=(10,20,30,40,50)
+# for i in a:
+#     print(i)
+
+##Q2
+# a=(10,20,30,40,50)
+# sum=0
+# for i in a:
+#     sum+=i
+#print(sum)
+
+##Q3
+# a=(10,30,20,50,40,90)
+# largest=a[0]
+# for i in a:
+#     if i>largest:
+#         largest=i
+# print(largest,"it a largest no.")
+        
+##Q4
+# a=(10,30,20,50,40,90)
+# smallest=a[0]
+# for i in a:
+#     if i<smallest:
+#        smallest=i
+# print(smallest,"it a smallest no.")
+
+##Q5
+# a = (10, 20, 10, 30, 10, 40)
+# target = 10
+# count=0
+# for i in a:
+#     if i==target:
+#         count+=1
+# print(count)
+
+##Q6
+# a = (10, 20, 30, 40, 50)
+# target = 30
+# if target in a:
+#         print("present")
+# else:
+#     print("not present")
+
+##Q7
+# a = (10, 15, 20, 25, 30, 35)
+# new_list=[]
+# for i in a:
+#     if i%2==0:
+#         new_list.append(i)
+# print(new_list)
+
+##Q8
+# a = (10, 20, 30, 40)
+# b=list(a)
+# print(b)
+
+##Q9
+# a = (10, 20, 30, 10, 40, 20, 50)
+# seen=set()
+# for i in a:
+#     if i in seen:
+#         print(i)
+#     else:
+#         seen.add(i)
+    
+##Q10
+# a = (10, 20, 30, 40, 50)
+# n=len(a)
+# rev=()
+# for i in range(n-1,-1,-1):
+#     rev+=(a[i],)
+# print(rev)
+    
+
+##Q11
+# tuple=immutable ,it show error
+# list =mutable,it change digit
+
+##Q12
+# a = (10, 20, 10, 30, 40, 20, 50, 10)
+# largest=a[0]
+# for i in a:
+#     if i>largest:
+#         largest=i
+# print(largest,"it a largest no.")
+# smallest=a[0]
+# for i in a:
+#     if i<smallest:
+#        smallest=i
+# print(smallest,"it a smallest no.")
